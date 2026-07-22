d82b052b31c4db5a55f0b48daf6c7717b139701b
ICAROCAETANO <icaro.caetano@gmail.com>
2026-07-21 22:10:14 -0300

    first commit

---
+++ src/main/java/com/projectjava25/poc_class_file_api/PocClassFileApiApplication.java
@@ -0,0 +1,13 @@
+package com.projectjava25.poc_class_file_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocClassFileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocClassFileApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectjava25/poc_class_file_api/PocClassFileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjava25.poc_class_file_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocClassFileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
