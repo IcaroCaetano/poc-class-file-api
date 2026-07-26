a3c1daa2946fd21c6c3e5341ce456199c78d7d73
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-07-26 20:03:24 -0300

    Atualizar o PocClassFileApiApplication.java

--- src/main/java/com/projectjava25/poc_class_file_api/PocClassFileApiApplication.java
+++ src/main/java/com/projectjava25/poc_class_file_api/PocClassFileApiApplication.java
@@ -3,11 +3,11 @@ package com.projectjava25.poc_class_file_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class PocClassFileApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocClassFileApiApplication.class, args);
+Example01List.execute();
 	}
 
 }
